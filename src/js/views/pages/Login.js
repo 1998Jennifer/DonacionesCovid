40970c27c7cad0be1c23b73ed5892d89b5cf01e7
Jennifer Intriago <jenniferintriago28@outlook.es>
2020-12-28 00:51:44 -0500

    Agregar estilos de inicio de sesion/registro

--- src/js/views/pages/Login.js
+++ src/js/views/pages/Login.js
@@ -6,6 +6,7 @@
 
 // Autocompletado y sintaxis de HTML para los template literals
 import { html } from 'lit-html';
+import '../../../css/login.css';
 
 const LogIn = {
   /**
@@ -14,8 +15,85 @@ const LogIn = {
    */
   getLogInTemplate: (): string => {
     const view = html`
-      <main>
-        <p>Login</p>
+      <main class="main-form">
+        <div class="form-container">
+          <h1>¡Bienvenido!</h1>
+          <form class="form-login">
+            <label for="Email">
+              <span>Email</span>
+              <div class="main-input-container">
+                <i class="fas fa-envelope"></i>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Ej. correo@dominio.com"
+                />
+              </div>
+            </label>
+            <label for="password">
+              <span>Contraseña</span>
+              <div class="main-input-container">
+                <i class="fas fa-lock"></i>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Escriba aquí..."
+                />
+                <i class="fas fa-eye"></i>
+              </div>
+            </label>
+            <input type="submit" value="Iniciar sesión" class="button-send" />
+          </form>
+          <div class="line">
+            <div></div>
+            <p>...o si quizás no tienes cuenta</p>
+            <div></div>
+          </div>
+        </div>
+
+        <div class="form-container">
+          <h1>Regístrate</h1>
+          <form class="form-login">
+            <label for="Email">
+              <span>Email</span>
+              <div class="main-input-container">
+                <i class="fas fa-envelope"></i>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Ej. correo@dominio.com"
+                />
+              </div>
+            </label>
+            <label for="usuario">
+              <span>Usuario</span>
+              <div class="main-input-container user">
+              <i class="fas fa-user"></i>
+              <input
+                  type="text"
+                  id="usuario"
+                  placeholder="Ej. usuario123"
+              />
+              </div>
+              <span class="span-letter">Máximo 16 caracteres</span>
+            </label>
+            <label for="password" class="label-pass">
+              <span>Contraseña</span>
+              <div class="main-input-container user">
+                <i class="fas fa-lock"></i>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Escriba aquí..."
+                />
+                <i class="fas fa-eye"></i>
+                </div>
+                <span class="span-letter">La contraseña debe contener al menos un caracter <br> especial, un número, una letra minúscula y mayúscula</span>
+            </label>
+            <input type="submit" value="Registrarse" class="button-send-register"/>
+          </form>
+          </div>
+        </div>
       </main>
     `;
     return view;
